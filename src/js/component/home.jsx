f894b1fc386ac1880743684d4b467d6c781528ae
mnkhoji <161701934+mnkhoji@users.noreply.github.com>
2024-05-13 19:35:35 +0000

    full changes Mike

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [selected, setSelected] = useState("")
+	const handleSelected = (color) => setSelected(color)
+	const [show,setShow] = useState(false)
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="box">
+				<div className={`${selected === 'red-circle'? "red-selected" : "red-circle"} red-circle`} onClick={()=> handleSelected("red-circle")}></div>
+				<div className={`${selected === 'orange-circle'? "orange-selected" : "orange-circle"} orange-circle`} onClick={()=> handleSelected("orange-circle")}></div>
+				<div className={`${selected === 'green-circle'? "green-selected" : "green-circle"} green-circle`} onClick={()=> handleSelected("green-circle")}></div>
+				<div className={show?`${selected === 'purple-circle'? "purple-selected" : "purple-circle"} purple-circle`:null} onClick={()=> handleSelected("purple-circle")}></div>
+			</div>
+			
+			<button className="btn btn-primary" onClick={()=>setShow(!show)} >Show</button>
+    		
+			
 		</div>
 	);
 };
